dd1a002a3e4f1a8c246acff66c3c0a9ae1a6d0ec
tygoli <tylerlindsey403@gmail.com>
2022-06-28 20:06:46 -0600

    the current form will create a tuple from user input, but does not add to DB

--- app7.py
+++ app7.py
@@ -1,16 +1,11 @@
 import dbcreds
 import mariadb
 
-conn = mariadb.connect(
-  user=dbcreds.user,
-  password=dbcreds.password,
-  host=dbcreds.host,
-  port=dbcreds.port,
-  database=dbcreds.database
-  )
 
-cursor = conn.cursor()
+# name = []
+# email = []
 
+# users = list(zip(name, email))
 users = []
 userInput = input('Enter your Name and then email:\n ')
 
@@ -19,19 +14,29 @@ while userInput != "*":
   # users.append(userInput)
   users.append(tuple(userInput.split()))
   userInput = input("Enter your Name and then email:\n ")
+  
+  # if userInput == "*":
+  #   for user in users:
+  #     conn = mariadb.connect(
+  #     user=dbcreds.user,
+  #     password=dbcreds.password,
+  #     host=dbcreds.host,
+  #     port=dbcreds.port,
+  #     database=dbcreds.database
+  #     )
+
+  #     cursor = conn.cursor()  
+  #     cursor.execute("INSERT INTO user (name, email) VALUES (?,?)", [])
+  #     if(cursor.rowcount == 1):
+  #       conn.commit()
+  #       print(f"Tyler you updated the db")
+  #     else:
+  #       print("error, not added")
 print(users)
 
-# for user in users:
-# name = input('enter name: ')
 
-  # cursor.execute("INSERT INTO user (name, email) VALUES (?,?)", [])
-  # if(cursor.rowcount == 1):
-  #   conn.commit()
-  #   print(f"Tyler you updated the db")
-  # else:
-  #   print("error, not added")
     
-conn.close
+# conn.close
 
 
 # need 1 array of tuples (users)
